3159024290578c249d99c063e6aa347bf8e85eb6
andrevinicius201 <32529232+andrevinicius201@users.noreply.github.com>
2019-08-12 14:52:02 -0300

    Add files via upload
    Usuários podem alterar suas configurações de perfil (nome de usuário e email vinculado à conta). Imagem de perfil default.  Mudanças de layout na página de perfil do usuário autenticado.

--- webapp_social_media/routes.py
+++ webapp_social_media/routes.py
@@ -1,48 +1,89 @@
-from flask import render_template, url_for, flash, redirect
-from webapp_social_media import app
-from webapp_social_media.forms import RegistrationForm, LoginForm
+from flask import render_template, url_for, flash, redirect, request
+from webapp_social_media import app, db, bcrypt
+from webapp_social_media.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from webapp_social_media.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
-posts=[
-	{
-	'author':'Andre Santos', 
-	'title':'Publicação de teste Web App', 
-	'content':'Primeiro conteúdo publicado', 
-	'date_posted':'7 de julho de 2019'
-	},
-	{
-	'author':'Gustavo Barros', 
-	'title':'Criando aplicações para Web', 
-	'content':'Segundo conteúdo publicado', 
-	'date_posted':'08 de agosto de 2019'
-	},
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
 ]
 
-@app.route('/')
-@app.route('/home')
+
+@app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-	form = RegistrationForm()
-	if form.validate_on_submit():
-		flash(f'Account created for {form.username.data}!', 'success')
-		return redirect(url_for('home'))
-	return render_template('register.html', title='Register', form=form)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-			flash('You have been logged in', 'success')
-			return redirect(url_for('home'))
-		else:
-			flash('Login unsuccessfull. Please check your username and password', 'danger')
-	return render_template('login.html', title='Login', form=form)
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+
+
+
