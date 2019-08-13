f27c3ea3147c571cee15aae60e703bb327fd82a2
andrevinicius201 <32529232+andrevinicius201@users.noreply.github.com>
2019-08-13 15:42:07 -0300

    Add files via upload
    Usuários podem alterar sua imagem de perfil. Implementação da biblioteca para redimensionamento da imagem de usuário, diminuindo o armazenamento necessário para essa informação.

--- webapp_social_media/routes.py
+++ webapp_social_media/routes.py
@@ -1,3 +1,6 @@
+from PIL import Image
+import secrets
+import os
 from flask import render_template, url_for, flash, redirect, request
 from webapp_social_media import app, db, bcrypt
 from webapp_social_media.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -67,11 +70,26 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+
 @app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
